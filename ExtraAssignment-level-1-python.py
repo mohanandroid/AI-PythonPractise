508dcd935b58f3c816a6fc1a305e16b2b473b95d
Mohankumar.S <mohankumar.s@mitie.com>
2025-11-26 14:35:05 +0530

    Added extraassignment

---
+++ ExtraAssignment-level-1-python.py
@@ -0,0 +1,53 @@
+#print Statement
+print("HOPE AI")
+
+# Buy input 
+input("Enter your institute name? :")
+
+#Buy inputs
+input("Enter your name: ")
+int(input("Enter your age: "))
+input("Enter your school name: ")
+input("Enter your degree: ")
+
+#Addition
+a= 43
+b= 34
+add= a+b
+print("add ", add)
+
+#Subtraction
+a= 43
+b= 34
+sub= a-b
+print("sub ", sub)
+
+#Multiplication
+a= 63
+b= 97
+mul= a*b
+print("mul ", mul)
+
+#Division
+a= 76
+b= 31
+flot_div= a/b
+print("Flot Div= ",flot_div)
+
+#Floor division
+a= 76
+b= 31
+flot_div=int(a/b)
+print("Flot Div= ",flot_div)
+
+#Modulo
+a= 76
+b= 31
+modulo = a % b
+print("Modulo= ",modulo)
+
+#Power
+a= 4
+b= 4
+power = a**b
+print("power= ",power)
