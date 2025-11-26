38f08a213c218d85f35a3ef9ff5a27d5c83cb7fc
Mohankumar.S <mohankumar.s@mitie.com>
2025-11-26 14:06:07 +0530

    Add statement.py

---
+++ statements.py
@@ -0,0 +1,16 @@
+
+age=int(input("Enter the age"))
+# if(age<18):
+#     print("Child")
+# else:
+#     print("Adult")
+
+
+if(age<18):
+    print("Child")
+elif(age<35):
+     print("Adult")
+elif(age<59):
+    print("Citizen")
+else:
+    print("Senior Citizen");    
